177decb5e2691c7c4bffa9efa42247c48fa04797
dynamic-henry-kwon <jukdo67@gmail.com>
2020-12-26 23:38:23 +0900

    20201226

---
+++ src/P2484.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+
+
+public class P2484 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+        int N = 0;
+        int R = 0;
+        N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int sum = 0;
+            int[] ia = new int[7];
+            String t = br.readLine();
+            for (int j = 0; j < t.length(); j++) {
+                if (t.charAt(j) != ' ') {
+                    ia[t.charAt(j) - 48]++;
+                }
+            }
+            int temp = 0;
+            int cone = 0;
+            for (int k = 1; k <= 6; k++) {
+                if (ia[k] == 4) {
+                    sum = 50000 + (k * 5000);
+                    break;
+                } else if (ia[k] == 3) {
+                    sum = 10000 + (k * 1000);
+                    break;
+                } else if (ia[k] == 2) {
+                    if (temp == 0) {
+                        sum = 1000 + (k * 100);
+                        temp = k;
+                    } else {
+                        sum = 2000 + (temp * 500) + (k * 500);
+                        break;
+                    }
+                } else if (ia[k] == 1) {
+                    cone++;
+                    if( cone == 4)
+                        sum = (k * 100);
+                }
+            }
+            //System.out.printf("sum: %d\n", sum);
+            if (R < sum)
+                R = sum;
+        }
+        System.out.println(R);
+    }
+}
---
+++ src/P2798.java
@@ -0,0 +1,83 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class P2798 {
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int N;
+    int[] V;
+    int[][] al;
+    boolean[] visitArr;
+    int M;
+    int max;
+
+    Main(int N, int[] V, int M) {
+        this.N = N;
+        al = new int[this.N + 1][this.N + 1];
+        visitArr = new boolean[this.N + 1];
+        this.V = V;
+        this.M = M;
+        this.max = 0;
+    }
+
+    void put(int x, int y) {
+        al[x][y] = 1;
+
+    }
+
+    void DFSUtil(int v, boolean[] visiatArr, int step, int sum) {
+        visiatArr[v] = true;
+        step++;
+        sum += V[v];
+        if (step == 3) {
+            if (sum <= M && sum > max) {
+                this.max = sum;
+            }
+            return;
+        }
+        for (int i = 1; i < al[v].length; i++) {
+            if ((al[v][i] == 1)) {
+                DFSUtil(i, visiatArr, step, sum);
+            }
+        }
+    }
+
+    void DFS(int v) {
+        DFSUtil(v, visitArr, 0, 0);
+    }
+
+    public static void main(String[] args) throws IOException {
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]), M = Integer.parseInt(input[1]);
+        input = br.readLine().split(" ");
+        int[] V = new int[N + 1];
+        for (int i = 0; i < N; i++) {
+            V[i + 1] = Integer.parseInt(input[i]);
+        }
+        // 설정부
+
+        Main bj = new Main(N, V, M);
+
+        for (int i = 1; i < V.length; i++) {
+            for (int j = i + 1; j < V.length; j++) {
+                if (i == 1 && j == V.length - 1) {
+                    continue;
+                }
+                bj.put(i, j);
+            }
+        }
+
+        for (int i = 1; i < V.length; i++) {
+            bj.DFS(i);
+        }
+
+        System.out.println(bj.max);
+
+    }
+
+}
