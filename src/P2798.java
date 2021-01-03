f67c6dc101008e588db8c35539b3eace66d831ed
dynamic-henry-kwon <jukdo67@gmail.com>
2021-01-03 12:49:40 +0900

    20210103 P2798 develop

--- src/P2798.java
+++ src/P2798.java
@@ -1,76 +1,60 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class P2798 {
 
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    int N;
-    int[] V;
-    int[][] al;
-    boolean[] visitArr;
-    int M;
-    int max;
+    int N;//카드수
+    int[] V;//카드담은 배열
+    int M;//기준값
+    int max;//최대값
 
     P2798(int N, int[] V, int M) {
         this.N = N;
-        al = new int[this.N + 1][this.N + 1];
-        visitArr = new boolean[this.N + 1];
         this.V = V;
         this.M = M;
         this.max = 0;
     }
 
-    void put(int x, int y) {
-        al[x][y] = 1;
-
-    }
-
-    void DFSUtil(int v, boolean[] visiatArr, int step, int sum) {
-        visiatArr[v] = true;
+    //배열 V 속의 v 번째 카드를 step + 1번째로 탐색하는 경우의 sum
+    void DFSUtil(int v, int step, int sum) {
+        // step + 1
         step++;
+        //그때의 합
         sum += V[v];
+        //마지막 3번째인경우
         if (step == 3) {
+            //최대값인지 확인하고 탐색 종료
             if (sum <= M && sum > max) {
                 this.max = sum;
             }
             return;
         }
-        for (int i = 1; i < al[v].length; i++) {
-            if ((al[v][i] == 1)) {
-                DFSUtil(i, visiatArr, step, sum);
-            }
+        //V 배열 내의 v번째카드 다음 카드를 뽑는 경우 탐색
+        for (int i = v + 1; i < N + 1; i++) {
+            DFSUtil(i, step, sum);
         }
     }
-
+    // 배열 V 속의 v번째 카드를 최초로 뽑는 경우부터 시작
     void DFS(int v) {
-        DFSUtil(v, visitArr, 0, 0);
+        DFSUtil(v, 0, 0);
     }
 
     public static void main(String[] args) throws IOException {
         String[] input = br.readLine().split(" ");
+        //카드 수와 기준값을 담는다.
         int N = Integer.parseInt(input[0]), M = Integer.parseInt(input[1]);
         input = br.readLine().split(" ");
         int[] V = new int[N + 1];
         for (int i = 0; i < N; i++) {
             V[i + 1] = Integer.parseInt(input[i]);
         }
-        // 설정부
-
+        // 객체 초기화
         P2798 bj = new P2798(N, V, M);
 
-        for (int i = 1; i < V.length; i++) {
-            for (int j = i + 1; j < V.length; j++) {
-                if (i == 1 && j == V.length - 1) {
-                    continue;
-                }
-                bj.put(i, j);
-            }
-        }
-
+        //배열의 1 번 카드를 뽑는 경우부터 탐색 시작
         for (int i = 1; i < V.length; i++) {
             bj.DFS(i);
         }
