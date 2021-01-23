64a9a469e325cdecb5fe3fa95ca351ab3504e167
dynamic-henry-kwon <jukdo67@gmail.com>
2021-01-23 18:15:56 +0900

    20210123 week 5

---
+++ src/P1759.java
@@ -0,0 +1,84 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class P1759 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int L, C;
+    static String[] inA;
+    static int[] isPossible;
+    static String[] result;
+    static String mo;
+    static String ja;
+    static int parseInt(String in) {
+        return Integer.parseInt(in);
+    }
+
+    static int checkMo(String[] result) {
+        for (int i = 0; i < result.length; i++) {
+            if (mo.indexOf(result[i]) != -1) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+    static int checkJa(String[] result) {
+        int output = 0;
+        for (int i = 0; i < result.length; i++) {
+            if (ja.indexOf(result[i]) != -1) {
+                output++;
+            }
+            if (output == 2)
+                return 1;
+        }
+        return -1;
+    }
+    static void bfs(int d) {
+        if (d == L) {
+            if (checkMo(result) == 1) {
+                if ( checkJa(result ) == 1 ) {
+                    for (int i = 0; i < d; i++) {
+                        System.out.print(result[i]);
+                    }
+                    System.out.println(); 
+                }
+ 
+            }
+            return;
+        }
+        
+        for (int i = 0; i < C; i++) {
+            if (isPossible[i] != 1) {
+                isPossible[i] = 1;
+                if (d == 0) {
+                    result[d] = inA[i];
+                    bfs(d + 1);
+                } else {
+                    if ((int) result[d - 1].charAt(0) < (int) inA[i].charAt(0)) {
+                        result[d] = inA[i];
+                        bfs(d + 1);
+                    }
+                }
+                isPossible[i] = 0;
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        String[] in = br.readLine().split(" ");
+        L = parseInt(in[0]);
+        C = parseInt(in[1]);
+        isPossible = new int[C];
+        inA = br.readLine().split(" ");
+        Arrays.sort(inA);
+        result = new String[L];
+        mo = "aeiou";
+        ja = "bcdfghjklmnpqrstvwxyz";
+        bfs(0);
+    }
+    
+}
---
+++ src/P3085.java
@@ -0,0 +1,127 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class P3085 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static String[][] iaa;
+    static int N;
+
+    static void switchArrR(int i, int j) {
+        if (iaa[i][j].equals(iaa[i][j + 1])) {
+            return;
+        }
+        String tmp = null;
+        tmp = iaa[i][j];
+        iaa[i][j] = iaa[i][j + 1];
+        iaa[i][j + 1] = tmp;
+
+        
+    }
+    
+    static void switchArrC(int i, int j) {
+        if (iaa[i][j].equals(iaa[i + 1][j])) {
+            return;
+        }
+        String tmp = iaa[i][j];
+        iaa[i][j] = iaa[i + 1][j];
+        iaa[i + 1][j] = tmp;
+    }
+    
+    static int searchR(int c, int r) {
+        int count = 0;
+        for (int i = r; i < N; i++) {
+            if (iaa[c][i].equals(iaa[c][r])) {
+                count++;
+            } else {
+                break;
+            }
+        }
+        for (int i = r - 1; i > 0; i--) {
+            if (iaa[c][i].equals(iaa[c][r])) {
+                count++;
+            } else {
+                break;
+            }
+        }
+        
+        return count;
+    }
+    
+    static int searchC(int c, int r) {
+        int count = 0;
+        for (int i = c; i < N; i++) {
+            if (iaa[i][r].equals(iaa[c][r])) {
+                count++;
+            } else {
+                break;
+            }
+        }
+        for (int i = c - 1; i > 0; i--) {
+            if (iaa[i][r].equals(iaa[c][r])) {
+                count++;
+            } else {
+                break;
+            }
+        }
+        return count;
+    }
+    
+    public static void main(String[] args) throws NumberFormatException, IOException {
+         N = Integer.parseInt(br.readLine());
+        iaa = new String[N][N];
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            iaa[i] = br.readLine().split("");
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                int result = searchR(i, j);
+                if (max < result)
+                    max = result;
+                result = searchC(i, j);
+                if (max < result)
+                    max = result;  
+            }
+        }
+        //ystem.out.println("max: " + max);
+        
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N - 1; j++) {
+                switchArrR(i, j);
+                for (int a = 0; a < N; a++) {
+                    for (int b = 0; b < N; b++) {
+                        int result = searchR(a, b);
+                        if (max < result)
+                            max = result;
+                        result = searchC(a, b);
+                        if (max < result)
+                            max = result;
+                    }
+                }
+                switchArrR(i, j);
+            }
+        }
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < N; j++) {
+                switchArrC(i, j);
+                for (int a = 0; a < N; a++) {
+                    for (int b = 0; b < N; b++) {
+                        int result = searchR(a, b);
+                        if (max < result)
+                            max = result;
+                        result = searchC(a, b);
+                        if (max < result)
+                            max = result;
+                    }
+                }
+                switchArrC(i, j);
+            }
+        }
+        System.out.println(max);
+    }
+}
